71aab3e9c94c129b7f669f39c7be8d1342b370b8
ciera.christopher <ciera.christopher@d760e6ef-b04e-0410-a379-4f5597c8b4f3>
2008-07-03 21:24:51 +0000

    added field initializers to the CFG

--- trunk/CrystalPlugin/src/edu/cmu/cs/crystal/cfg/eclipse/EclipseNodeFirstCFG.java
+++ trunk/CrystalPlugin/src/edu/cmu/cs/crystal/cfg/eclipse/EclipseNodeFirstCFG.java
@@ -28,27 +28,29 @@ import org.eclipse.jdt.core.dom.ForStatement;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.WhileStatement;
 
-public class EclipseNodeFirstCFG extends EclipseCFG
-{
-	public EclipseNodeFirstCFG()
-	{
+/**
+ * A CFG which places the control flow node at the merge point.
+ * 
+ * @author Ciera Jaspan
+ * 
+ */
+public class EclipseNodeFirstCFG extends EclipseCFG {
+	public EclipseNodeFirstCFG() {
 		super();
 	}
-	
-	public EclipseNodeFirstCFG(MethodDeclaration method)
-	{
+
+	public EclipseNodeFirstCFG(MethodDeclaration method) {
 		super(method);
 	}
-	
-	
+
 	@Override
 	public boolean visit(DoStatement node) {
 		EclipseCFGNode doBegin = nodeMap.get(node);
 		EclipseCFGNode doEnd = new EclipseCFGNode(null);
-		
+
 		blockStack.pushUnlabeled(node, doEnd, doBegin);
-		doBegin.setEnd(doEnd);	
-		
+		doBegin.setEnd(doEnd);
+
 		return true;
 	}
 
@@ -57,28 +59,28 @@ public class EclipseNodeFirstCFG extends EclipseCFG
 		EclipseCFGNode doEnd = nodeMap.get(node).getEnd();
 		EclipseCFGNode cond = nodeMap.get(node.getExpression());
 		EclipseCFGNode body = nodeMap.get(node.getBody());
-		EclipseCFGNode doBegin = nodeMap.get(node); 
-		
+		EclipseCFGNode doBegin = nodeMap.get(node);
+
 		createEdge(doBegin, body.getStart());
 		createEdge(body.getEnd(), cond.getStart());
 		createEdge(cond.getEnd(), doBegin, true);
 		createEdge(cond.getEnd(), doEnd, false);
 
 		blockStack.popUnlabeled();
-		
+
 		doBegin.setName("do");
 		doEnd.setName("od");
 	}
 
 	@Override
 	public boolean visit(EnhancedForStatement node) {
-		EclipseCFGNode eforBegin = nodeMap.get(node); 
+		EclipseCFGNode eforBegin = nodeMap.get(node);
 		EclipseCFGNode eforEnd = new EclipseCFGNode(null);
 
 		blockStack.pushUnlabeled(node, eforEnd, eforBegin);
-		
+
 		eforBegin.setEnd(eforEnd);
-		
+
 		return true;
 	}
 
@@ -86,35 +88,35 @@ public class EclipseNodeFirstCFG extends EclipseCFG
 	public void endVisit(EnhancedForStatement node) {
 		EclipseCFGNode list = nodeMap.get(node.getExpression());
 		EclipseCFGNode body = nodeMap.get(node.getBody());
-		EclipseCFGNode param = nodeMap.get(node.getParameter());				
-		EclipseCFGNode eforBegin = nodeMap.get(node); 
+		EclipseCFGNode param = nodeMap.get(node.getParameter());
+		EclipseCFGNode eforBegin = nodeMap.get(node);
 		EclipseCFGNode eforEnd = eforBegin.getEnd();
-			
+
 		eforBegin.setStart(list.getStart());
-		
+
 		createEdge(list.getEnd(), eforBegin);
 		createItrEdge(eforBegin, eforEnd, true);
-		createItrEdge(eforBegin, param.getStart(), false);	
+		createItrEdge(eforBegin, param.getStart(), false);
 		createEdge(param.getEnd(), body.getStart());
 		createEdge(body.getEnd(), eforBegin);
 
 		blockStack.popUnlabeled();
-		
+
 		eforBegin.setName("efor");
 		eforEnd.setName("rofe");
 	}
-	
+
 	@Override
 	public boolean visit(ForStatement node) {
-		EclipseCFGNode forBegin = nodeMap.get(node); 
+		EclipseCFGNode forBegin = nodeMap.get(node);
 		EclipseCFGNode forEnd = new EclipseCFGNode(null);
 
 		forBegin.setName("forBdummy");
 		forEnd.setName("forEdummy");
 
 		blockStack.pushUnlabeled(node, forEnd, forBegin);
-		forBegin.setEnd(forEnd);		
-		
+		forBegin.setEnd(forEnd);
+
 		return true;
 	}
 
@@ -125,16 +127,16 @@ public class EclipseNodeFirstCFG extends EclipseCFG
 		EclipseCFGNode body = nodeMap.get(node.getBody());
 		EclipseCFGNode endDummy = beginDummy.getEnd();
 		EclipseCFGNode current, last = null;
-		
-		//hijack the node map
-		//make our REAL for node, the old one was a dummy point for continues
+
+		// hijack the node map
+		// make our REAL for node, the old one was a dummy point for continues
 		EclipseCFGNode forNode = new EclipseCFGNode(node);
 		nodeMap.put(node, forNode);
 		beginDummy.setASTNode(null);
 		beginDummy.setEnd(beginDummy);
-		
-		//connect up initializers
-		for (ASTNode init : (List<ASTNode>)node.initializers()) {
+
+		// connect up initializers
+		for (ASTNode init : (List<ASTNode>) node.initializers()) {
 			current = nodeMap.get(init);
 			if (last != null)
 				createEdge(last.getEnd(), current.getStart());
@@ -142,74 +144,67 @@ public class EclipseNodeFirstCFG extends EclipseCFG
 				forNode.setStart(current.getStart());
 			last = current;
 		}
-		
-		//connect initializers to the for node
+
+		// connect initializers to the for node
 		if (last != null)
 			createEdge(last.getEnd(), forNode);
 
-		
-		//connect for node to the conditional
+		// connect for node to the conditional
 		if (cond != null) {
 			createEdge(forNode, cond.getStart());
 			createEdge(cond.getEnd(), endDummy, false);
 			createEdge(cond.getEnd(), body.getStart(), true);
-		}
-		else {
+		} else {
 			createEdge(forNode, body.getStart());
 		}
-			
-		
-		//body is now conteced to the beginDummy, which is where the continue
-		//jumps will go to
+
+		// body is now conteced to the beginDummy, which is where the continue
+		// jumps will go to
 		createEdge(body.getEnd(), beginDummy);
-		
+
 		last = beginDummy;
-		for (ASTNode update : (List<ASTNode>)node.updaters()) {
+		for (ASTNode update : (List<ASTNode>) node.updaters()) {
 			current = nodeMap.get(update);
 			createEdge(last.getEnd(), current.getStart());
 			last = current;
 		}
 
-		//the final back edge
-		createEdge(last.getEnd(), forNode);	
+		// the final back edge
+		createEdge(last.getEnd(), forNode);
 
 		blockStack.popUnlabeled();
-		
+
 		forNode.setName("for");
 		forNode.setEnd(endDummy);
 	}
-	
-	
+
 	@Override
 	public boolean visit(WhileStatement node) {
-		EclipseCFGNode whileBegin = nodeMap.get(node); 
+		EclipseCFGNode whileBegin = nodeMap.get(node);
 		EclipseCFGNode whileEnd = new EclipseCFGNode(null);
-		
+
 		blockStack.pushUnlabeled(node, whileEnd, whileBegin);
 		whileBegin.setEnd(whileEnd);
-		
+
 		return true;
 	}
-	
+
 	@Override
 	public void endVisit(WhileStatement node) {
 		EclipseCFGNode cond = nodeMap.get(node.getExpression());
 		EclipseCFGNode body = nodeMap.get(node.getBody());
 		EclipseCFGNode whileBegin = nodeMap.get(node);
 		EclipseCFGNode whileEnd = whileBegin.getEnd();
-		
-		
+
 		createEdge(whileBegin, cond.getStart());
 		createEdge(cond.getEnd(), whileEnd, false);
 		createEdge(cond.getEnd(), body.getStart(), true);
 		createEdge(body.getEnd(), whileBegin);
-		
+
 		blockStack.popUnlabeled();
-		
+
 		whileBegin.setName("while");
 		whileEnd.setName("elihw");
 	}
 
-	
-	
 }
