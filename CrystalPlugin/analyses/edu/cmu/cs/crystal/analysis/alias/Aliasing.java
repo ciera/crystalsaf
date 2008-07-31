38a79424020ec8f83e6933648d6efc012ded480c
kevin.bierhoff <kevin.bierhoff@d760e6ef-b04e-0410-a379-4f5597c8b4f3>
2008-07-31 21:58:21 +0000

    minor javadoc fix

--- CrystalPlugin/analyses/edu/cmu/cs/crystal/analysis/alias/Aliasing.java
+++ CrystalPlugin/analyses/edu/cmu/cs/crystal/analysis/alias/Aliasing.java
@@ -25,7 +25,7 @@ import java.util.Set;
  * This interface represents aliasing information about a program variable.
  * The current interface pretty much forces to represent this information as
  * a set of {@link ObjectLabel}s that the variable may point to.  Implementations
- * of this interface should override {{@link #equals(Object)} and {@link #hashCode()}.
+ * of this interface should override {@link #equals(Object)} and {@link #hashCode()}.
  * @author Kevin Bierhoff
  *
  */
