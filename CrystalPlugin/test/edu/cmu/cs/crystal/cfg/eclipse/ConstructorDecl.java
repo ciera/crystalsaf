0a6bab1f5d99c8510f2d0f72019a8b6499716c56
ciera.christopher <ciera.christopher@d760e6ef-b04e-0410-a379-4f5597c8b4f3>
2008-08-01 13:29:47 +0000

    Fixed Bug #2

--- CrystalPlugin/test/edu/cmu/cs/crystal/cfg/eclipse/ConstructorDecl.java
+++ CrystalPlugin/test/edu/cmu/cs/crystal/cfg/eclipse/ConstructorDecl.java
@@ -1,21 +1,19 @@
 /**
- * Copyright (c) 2006, 2007, 2008 Jonathan Aldrich, Nels E. Beckman,
- * Kevin Bierhoff, David Dickey, Ciera Jaspan, Thomas LaToza, Gabriel Zenarosa, and others.
- *
+ * Copyright (c) 2006, 2007, 2008 Jonathan Aldrich, Nels E. Beckman, Kevin Bierhoff, David Dickey,
+ * Ciera Jaspan, Thomas LaToza, Gabriel Zenarosa, and others.
+ * 
  * This file is part of Crystal.
- *
- * Crystal is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Crystal is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Crystal.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Crystal is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * Crystal is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with Crystal. If
+ * not, see <http://www.gnu.org/licenses/>.
  */
 package edu.cmu.cs.crystal.cfg.eclipse;
 
@@ -40,17 +38,14 @@ public class ConstructorDecl {
 
 	@BeforeClass
 	public static void setup() throws CoreException {
-		CompilationUnit compUnit = CFGTestUtils.parseCode(ConstructorDecl.class
-				.getName());
+		CompilationUnit compUnit = CFGTestUtils.parseCode(ConstructorDecl.class.getName());
 		decls = WorkspaceUtilities.scanForMethodDeclarationsFromAST(compUnit);
 	}
 
 	@Test
 	public void emptyTest() throws Exception {
 		for (MethodDeclaration decl : decls) {
-			if (decl.isConstructor()
-					&& decl.getName().getIdentifier()
-							.equals("ConstructorEmpty")) {
+			if (decl.isConstructor() && decl.getName().getIdentifier().equals("ConstructorEmpty")) {
 				Assert.assertTrue(CFGTestUtils.testAndCompareCFG(decl));
 				break;
 			}
@@ -60,15 +55,13 @@ public class ConstructorDecl {
 	public class ConstructorEmpty {
 		String field = "foo";
 
-		public ConstructorEmpty() {
-		}
+		public ConstructorEmpty() {}
 	}
 
 	@Test
 	public void superTest1() throws Exception {
 		for (MethodDeclaration decl : decls) {
-			if (decl.isConstructor()
-					&& decl.getName().getIdentifier().equals("ConstructorSub1")) {
+			if (decl.isConstructor() && decl.getName().getIdentifier().equals("ConstructorSub1")) {
 				Assert.assertTrue(CFGTestUtils.testAndCompareCFG(decl));
 				break;
 			}
@@ -88,8 +81,7 @@ public class ConstructorDecl {
 	@Test
 	public void superTest2() throws Exception {
 		for (MethodDeclaration decl : decls) {
-			if (decl.isConstructor()
-					&& decl.getName().getIdentifier().equals("ConstructorSub2")) {
+			if (decl.isConstructor() && decl.getName().getIdentifier().equals("ConstructorSub2")) {
 				Assert.assertTrue(CFGTestUtils.testAndCompareCFG(decl));
 				break;
 			}
@@ -99,6 +91,7 @@ public class ConstructorDecl {
 	public class ConstructorSub2 extends ConstructorEmpty {
 		String field2 = "foo";
 		String field3 = "bar";
+		static final String field4 = "static";
 
 		public ConstructorSub2(int x) {
 			super();
@@ -108,9 +101,7 @@ public class ConstructorDecl {
 	@Test
 	public void thisTest1() throws Exception {
 		for (MethodDeclaration decl : decls) {
-			if (decl.isConstructor()
-					&& decl.getName().getIdentifier()
-							.equals("ConstructorSelf1")) {
+			if (decl.isConstructor() && decl.getName().getIdentifier().equals("ConstructorSelf1")) {
 				List<SingleVariableDeclaration> params = decl.parameters();
 				if (params.get(0).getName().getIdentifier().equals("dx")) {
 					Assert.assertTrue(CFGTestUtils.testAndCompareCFG(decl));
@@ -135,9 +126,7 @@ public class ConstructorDecl {
 	@Test
 	public void thisTest2() throws Exception {
 		for (MethodDeclaration decl : decls) {
-			if (decl.isConstructor()
-					&& decl.getName().getIdentifier()
-							.equals("ConstructorSelf2")) {
+			if (decl.isConstructor() && decl.getName().getIdentifier().equals("ConstructorSelf2")) {
 				List<SingleVariableDeclaration> params = decl.parameters();
 				if (params.get(0).getName().getIdentifier().equals("sx")) {
 					Assert.assertTrue(CFGTestUtils.testAndCompareCFG(decl));
