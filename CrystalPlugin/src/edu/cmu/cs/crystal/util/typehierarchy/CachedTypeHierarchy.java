4e91cf23cc969a987f434388c5d3cdc3f4ae63ff
ciera.christopher <ciera.christopher@d760e6ef-b04e-0410-a379-4f5597c8b4f3>
2010-11-08 15:15:40 +0000

    More bug fixes to type hierarchy to make it faster.

--- CrystalPlugin/src/edu/cmu/cs/crystal/util/typehierarchy/CachedTypeHierarchy.java
+++ CrystalPlugin/src/edu/cmu/cs/crystal/util/typehierarchy/CachedTypeHierarchy.java
@@ -1,7 +1,11 @@
 package edu.cmu.cs.crystal.util.typehierarchy;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
 
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IType;
@@ -28,7 +32,10 @@ public class CachedTypeHierarchy implements TypeHierarchy {
 	public boolean existsCommonSubtype(String t1, String t2, boolean skipCheck1, boolean skipCheck2) {
 		if (t1.equals("java.lang.Object") || t2.equals("java.lang.Object"))
 			return true;
-		
+
+		if (t1.equals("void") || t2.equals("void"))
+			return false;
+
 		int genStart1 = t1.indexOf('<');
 		int genStart2 = t2.indexOf('<');
 		String type1, gen1, type2, gen2;
@@ -104,22 +111,28 @@ public class CachedTypeHierarchy implements TypeHierarchy {
 		if (list1.equals("") || list2.equals(""))
 			return true;
 		
-		String[] gens1 = list1.split(",");
-		String[] gens2 = list2.split(",");
+		List<List<Integer>> bracketPairs1 = bracketIndices(list1);
+		List<List<Integer>> bracketPairs2 = bracketIndices(list2);
 		
-		if (gens1.length != gens2.length)
+		List<String> gen1 = commaSplit(list1, bracketPairs1);
+		List<String> gen2 = commaSplit(list2, bracketPairs2);
+				
+		if (gen1.size() != gen2.size())
 			return false;
 		
-		for (int ndx = 0; ndx < gens1.length; ndx++) {
-			if (!existsCommonSubtype(gens1[ndx].trim(), gens2[ndx].trim()))
+		for (int ndx = 0; ndx < gen1.size(); ndx++) {
+			if (!existsCommonSubtype(gen1.get(ndx).trim(), gen2.get(ndx).trim()))
 				return false;
 		}
-		return true;	
+		return true;
 	}
 
 	public boolean isSubtypeCompatible(String subType, String superType) {
 		if (superType.equals("java.lang.Object"))
 			return true;
+		
+		if (subType.equals("void") || superType.equals("void"))
+			return false;
 		
 		int genStartSub = subType.indexOf('<');
 		int genStartSuper = superType.indexOf('<');
@@ -175,20 +188,88 @@ public class CachedTypeHierarchy implements TypeHierarchy {
 		if (listSub.equals(""))
 			return false;
 
-		String[] genSub = listSub.split(",");
-		String[] genSuper = listSuper.split(",");	
+		List<List<Integer>> bracketPairsSub = bracketIndices(listSub);
+		List<List<Integer>> bracketPairsSuper = bracketIndices(listSuper);
 		
-		if (genSub.length != genSuper.length)
+		List<String> genSub = commaSplit(listSub, bracketPairsSub);
+		List<String> genSuper = commaSplit(listSuper, bracketPairsSuper);
+				
+		if (genSub.size() != genSuper.size())
 			return false;
 		
-		for (int ndx = 0; ndx < genSub.length; ndx++) {
-			if (!isSubtypeCompatible(genSub[ndx].trim(), genSuper[ndx].trim()))
+		for (int ndx = 0; ndx < genSub.size(); ndx++) {
+			if (!isSubtypeCompatible(genSub.get(ndx).trim(), genSuper.get(ndx).trim()))
 				return false;
 		}
 		return true;	
 	}
 
+	private List<List<Integer>> bracketIndices(String s) {
+		List<List<Integer>> pairsOfIndices = new ArrayList<List<Integer>>();
+		Stack<Integer> stack = new Stack<Integer>(); // stack of open brackets
+		int current = 0;
+		while (current < s.length()) {
+			int openIndex = s.indexOf('<', current);
+			int closeIndex = s.indexOf('>', current);
+			if (openIndex > 0 && closeIndex > 0) { // both found
+				if (openIndex < closeIndex) { // open comes first
+					stack.push(openIndex);
+					current = openIndex+1;
+				} else { // close comes first
+					List<Integer> pair = new LinkedList<Integer>();
+					pair.add(stack.pop()); // open
+					pair.add(closeIndex); // close
+					pairsOfIndices.add(pair);
+					current = closeIndex+1;
+				}
+			} else if (closeIndex > 0) { // only close found
+				List<Integer> pair = new LinkedList<Integer>();
+				pair.add(stack.pop()); // open
+				pair.add(closeIndex); // close
+				pairsOfIndices.add(pair);
+				current = closeIndex+1;
+			} else if (openIndex > 0) { // only open found --> something went wrong
+				throw new RuntimeException("Error while parsing generics: "+s);
+			} else { // no (more) brackets found
+				assert(stack.isEmpty());
+				break;
+			}
+		}
+		return pairsOfIndices;
+	}
 	
+	/**
+	 * Split at commas, but exclude commas in the given ranges.
+	 * @return
+	 */
+	private List<String> commaSplit(String s, List<List<Integer>> excludedRanges) {
+		List<Integer> commas = new ArrayList<Integer>();
+		int current = 0;
+		while (current < s.length()) {
+		 	int index = s.indexOf(',', current);
+		 	if (index < 0) break;
+		 	boolean excluded = false;
+		 	for (List<Integer> pair : excludedRanges) {
+		 		if (index >= pair.get(0) & index <= pair.get(1)) {
+		 			excluded = true;
+		 			break;
+		 		}
+			}
+		 	if (!excluded) commas.add(index);
+		 	current = index+1;
+		}
+		
+		List<String> splitted = new ArrayList<String>();
+		current = 0;
+		for (Integer commaIndex : commas) {
+			splitted.add(s.substring(current, commaIndex));
+			current = commaIndex+1;
+		}
+		splitted.add(s.substring(current, s.length()));
+		
+		return splitted;
+	}
+
 	/**
 	 * 
 	 * @param qName
@@ -200,8 +281,12 @@ public class CachedTypeHierarchy implements TypeHierarchy {
 			IType baseType = project.findType(qName);
 			
 			if (baseType != null) {
-				ITypeHierarchy hierarchy = baseType.newTypeHierarchy(null);		
+				ITypeHierarchy hierarchy = baseType.newTypeHierarchy(project, null);		
 				addInHierarchy(baseType, hierarchy);
+				
+				for (IType iType : hierarchy.getRootInterfaces())
+					addInHierarchy(iType, hierarchy);
+				
 				
 				//Yeah...that just wasted a bunch of resources. Clean up now...
 				Runtime r = Runtime.getRuntime();
@@ -221,30 +306,20 @@ public class CachedTypeHierarchy implements TypeHierarchy {
 		String qName = type.getFullyQualifiedName('.');
 		TypeNode node = getOrCreateType(qName);
 		
+		if (node.isCompleted())
+			return;
+		
 		//Recurse on children
 		for (IType sub : hierarchy.getSubtypes(type)) {
 			String subName = sub.getFullyQualifiedName('.');
 			TypeNode subNode = getOrCreateType(subName);
 			
-			if (!subNode.isSupertype(node)) { //missing the connection
+			if (!subNode.isDirectSupertype(node)) { //missing the connection
 				node.addSubtype(subNode);
 				subNode.addSupertype(node);
 				addInHierarchy(sub, hierarchy);
 			}
 			//and if we already have the connection, we also have them all the way down.
-		}
-
-		//Recurse on parents
-		for (IType sup : hierarchy.getAllSupertypes(type)) {
-			String supName = sup.getFullyQualifiedName('.');
-			TypeNode supNode = getOrCreateType(supName);
-			
-			if (!node.isSupertype(supNode)) { //missing the connection
-				supNode.addSubtype(node);
-				node.addSupertype(supNode);
-				addInHierarchy(sup, hierarchy);
-			}
-			//and if we already have the connection, we also have them all the way up.
 		}
 	}
 
